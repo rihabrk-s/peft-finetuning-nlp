b835baff33b7d197401676424b96d3590d594f20
Rihab <kroumirihab@gmail.com>
2025-11-11 21:18:39 +0100

    preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,5 +1,5 @@
 import os
-import json
+import  json
 import pandas as pd
 from io import StringIO
 
@@ -8,7 +8,7 @@ OUTPUT_DIR = "data/cleaned/reports"
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-print(f"📂 Loading JSON file: {RAW_PATH}")
+print(f" Loading JSON file: {RAW_PATH}")
 with open(RAW_PATH, "r", encoding="utf-8") as f:
     data = json.load(f)
 
@@ -16,7 +16,7 @@ with open(RAW_PATH, "r", encoding="utf-8") as f:
 dfs = {}
 for key, content in data.items():
     if key.endswith(".txt"):
-        print(f"➡️ Converting {key} to DataFrame...")
+        print(f" Converting {key} to DataFrame...")
         dfs[key] = pd.read_csv(StringIO(content), sep="\t")
 
 # Sauvegarder chaque DataFrame
@@ -24,6 +24,6 @@ for key, df in dfs.items():
     name = key.replace(".txt", "")
     path = os.path.join(OUTPUT_DIR, f"{name}.csv")
     df.to_csv(path, index=False)
-    print(f"✅ Saved {path} ({df.shape[0]} rows, {df.shape[1]} columns)")
+    print(f" Saved {path} ({df.shape[0]} rows, {df.shape[1]} columns)")
 
 print("\n All financial tables extracted and saved successfully!")
